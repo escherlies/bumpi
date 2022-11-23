2a505d130fc86cc5fcd22413bc53febf99a69e5b
Enrico Scherlies <e.scherlies@pm.me>
2022-11-23 23:45:37 +0100

    Add docs, rename to bumpi

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
+# Bumpi - a get next semantic version bump cli tool
 
+Interactivly get the next version based on your latest git tag and your last commits:
 
+![Usage](./docs/Usage.png)
 
-# Notes 
+That's it!
 
-Using HLS 1.8.0.0 and GHC 9.2.4 https://github.com/haskell/haskell-language-server/issues/3241
+# Usage
+
+Binaries available soon!
+
+```bash
+cabal install
+
+get-next-version
+```
+
+# Motivation
+
+> Write programs that do one thing and do it well. [Unix philosophy](https://en.wikipedia.org/wiki/Unix_philosophy)
+
+I tried conventional commits with a tool that generates the next version. I did not like conventional commits after all since they are for machines, not humans.
+
+I wanted a tool that just bumps my version based on my git. But all of the tools out there are fully featured and highly specialized tools that do a bunch of stuff. For example:
+
+  - [mroth/bump](https://github.com/mroth/bump) Can only be used with GitHub repos.
+  - [sindresorhus/np](https://github.com/sindresorhus/np) For publishing NPM packages.
+  - [thenativeweb/get-next-version](https://github.com/thenativeweb/get-next-version) Uses conventional commits.
+  - [c4urself/bump2version](https://github.com/c4urself/bump2version) For Python.
+
+So bumpi does just one thing and that's it.
+
--- app/Main.hs
+++ app/Main.hs
@@ -6,27 +6,76 @@ import Data.String (IsString (fromString))
 import Data.Text (unpack)
 import qualified Data.Text as T
 import System.Process (readProcess)
-import Version (bump, showKeywords)
+import Version (Version, bump, showKeywords)
 
 
-main :: IO ()
-main =
-  do
-    nextVersion <- getNextVersionInteractive
-    writeFile "VERSION" nextVersion
+main :: IO T.Text
+main = getNextVersionInteractive
 
 
-getNextVersionInteractive :: IO String
+getNextVersionInteractive :: IO T.Text
 getNextVersionInteractive = do
-  v <- T.strip <$> readProcessT "git" (words "describe --tags --abbrev=0") ""
+  -- Get latest git tag
+  v <- T.strip <$> readProcessAsText "git" (words "describe --tags --abbrev=0") ""
   --     ^ strip newline
 
-  commits <- readProcessT "bash" ["-c", "git log $(git describe --tags --abbrev=0)..HEAD --oneline --pretty=\"%s\""] ""
-
+  -- Get latest commits for the user to review
+  commits <- readProcessAsText "bash" ["-c", "git log $(git describe --tags --abbrev=0)..HEAD --oneline --pretty=\"%s\""] ""
   printFriendlyUserMessage v commits
-  bt <- getBumpType
-  let bumped = bump (fromString bt) (fromString $ unpack v)
 
+  -- Ask the user for the bump version arity
+  userInput <- askForBumpArity
+
+  -- Handle user input
+  case userInput of
+    "q" -> bye
+    ":q" -> bye
+    _ ->
+      do
+        -- Actually bump the version!
+        let bumped = bump (fromString userInput) (fromString $ unpack v)
+        showBumpedMessage v bumped
+        writeFile "VERSION" (show bumped)
+        return $ fromString $ show bumped
+
+
+readProcessAsText :: FilePath -> [String] -> String -> IO T.Text
+readProcessAsText cmd arg stdin = T.pack <$> readProcess cmd arg stdin
+
+
+-- User IO
+
+askForBumpArity :: IO String
+askForBumpArity = do
+  Cli.putStyledLn
+    []
+    [ "Bump next version to "
+    , Cli.el
+        [Cli.dim]
+        ("(" <> fromString showKeywords <> ")")
+    , ":"
+    ]
+
+  getLine
+
+
+bye :: IO T.Text
+bye = return $ Cli.layout [Cli.fgColor Cli.Yellow] "Ok, aborting..."
+
+
+printFriendlyUserMessage :: T.Text -> T.Text -> IO ()
+printFriendlyUserMessage v cs =
+  Cli.putLines
+    [ "Current version:"
+    , Cli.el [Cli.fgColor Cli.Blue] ("  " <> v)
+    , ""
+    , "Commits since last version"
+    , Cli.el [] (T.unlines $ ("  " <>) <$> T.lines cs)
+    ]
+
+
+showBumpedMessage :: T.Text -> Version -> IO ()
+showBumpedMessage v bumped = do
   -- Clear user input
   Cli.putLines
     [ Cli.moveUp 2
@@ -40,35 +89,4 @@ getNextVersionInteractive = do
     , ""
     , el [Cli.fgColor Cli.Green] "*** Saved to ./VERSION! ***"
     , ""
-    ]
-
-  return (show bumped)
-
-
-getBumpType :: IO String
-getBumpType = do
-  Cli.putStyledLn
-    []
-    [ "Bump next version to "
-    , Cli.el
-        [Cli.dim]
-        ("(" <> fromString showKeywords <> ")")
-    , ":"
-    ]
-
-  getLine
-
-
-readProcessT :: FilePath -> [String] -> String -> IO T.Text
-readProcessT cmd arg stdin = T.pack <$> readProcess cmd arg stdin
-
-
-printFriendlyUserMessage :: T.Text -> T.Text -> IO ()
-printFriendlyUserMessage v cs =
-  Cli.putLines
-    [ "Current version:"
-    , Cli.el [Cli.fgColor Cli.Blue] ("  " <> v)
-    , ""
-    , "Commits since last version"
-    , Cli.el [] (T.unlines $ ("  " <>) <$> T.lines cs)
     ]
